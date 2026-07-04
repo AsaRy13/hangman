16b96ff2d20fe0b55adff4396feae9d34e13bfac
Asa Ryan Fletcher <167818887+AsaRy13@users.noreply.github.com>
2026-07-04 13:16:37 -0600

    Finished the hangman game and added README.md.

---
+++ README.md
@@ -0,0 +1,31 @@
+# Overview
+
+This program is a simple hangman game.
+
+I wrote this program as a way to learn C++ and this is my first program in C++ that wasn't a Hello World program.
+
+Here is a link to a video of me demoing this program:
+
+[Software Demo Video](https://youtu.be/kHv4fjjL3LI)
+
+# Development Environment
+
+Visual Studio
+
+C++ 20
+
+Libraries:
+* iostream
+* string
+* vector
+* set
+* array
+
+# Useful Websites
+
+- [W3Schools C++](https://www.w3schools.com/cpp/default.asp)
+- [C++ Containers](https://cplusplus.com/reference/stl/)
+
+# Future Work
+
+- Make it so you just have to press enter for pausing for messages rather than having to type a character first.
--- hangman/hangman.cpp
+++ hangman/hangman.cpp
@@ -2,10 +2,291 @@
 //
 
 #include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+#include <array>
+
+class PhraseLetter {
+private:
+    bool hidden = true;
+    char letter;
+
+public:
+    PhraseLetter(char letter) {
+        this->letter = letter;
+    }
+
+    char getLetter() const {
+        return this->letter;
+    }
+
+    void setHidden(bool state) {
+        this->hidden = state;
+    }
+
+    bool getHidden() const {
+        return this->hidden;
+    }
+    
+    char printLetter() const {
+        if (this->hidden) {
+            return '_';
+        }
+        else {
+            return this->letter;
+        }
+    }
+};
+
+class PhraseWord {
+private:
+    std::string word;
+    std::vector<PhraseLetter> letterClasses;
+    std::set<char> letters;
+
+public:
+    PhraseWord(const std::string& word) {
+        this->word = word;
+        for (char letter : word) {
+            if (letter != ' ') {
+                PhraseLetter letterClass(letter);
+                letterClasses.push_back(letterClass);
+                if (!letters.contains(letter)) {
+                    letters.insert(letter);
+                }
+            }
+        }
+    }
+
+    std::string display() {
+        std::string partWord;
+        for (const PhraseLetter& letter : letterClasses) {
+            partWord += letter.printLetter();
+        }
+        partWord += " ";
+        return partWord;
+    }
+
+    int guess(char letter) {
+        if (letters.contains(letter)) {
+            for (PhraseLetter& l : letterClasses) {
+                if (l.getLetter() == letter) {
+                    l.setHidden(false);
+                }
+            }
+            return 0;
+        }
+        return 1;
+    }
+    
+    bool allFound() {
+        for (const PhraseLetter& l : letterClasses) {
+            if (l.getHidden()) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+class PhraseClass {
+private:
+    std::string phrase;
+    std::vector<PhraseWord> words;
+
+public:
+    PhraseClass(const std::string& phrase) {
+        this->phrase = phrase;
+        std::string partPhrase = phrase;
+        while (partPhrase != "") {
+            auto partLocation = partPhrase.find(' ');
+            if (partLocation != std::string::npos) {
+                PhraseWord word(partPhrase.substr(0, partLocation));
+                words.push_back(word);
+                partPhrase.erase(0, partLocation + 1);
+            }
+            else {
+                PhraseWord word(partPhrase.substr(0, partPhrase.size()));
+                words.push_back(word);
+                partPhrase.erase();
+            }
+        }
+    }
+
+    std::string display() {
+        std::string partPhrase;
+        for (PhraseWord& word : words) {
+            partPhrase += word.display();
+        }
+        return partPhrase;
+    }
+
+    int guess(char letter) {
+        std::vector<int> output;
+        for (PhraseWord& word : words) {
+            output.push_back(word.guess(letter));
+        }
+        auto result = std::find(output.begin(), output.end(), 0);
+        if (result != output.end()) {
+            return 0;
+        }
+        return 1;
+    }
+
+    bool allFound() {
+        for (PhraseWord& word : words) {
+            if (!word.allFound()) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+std::string hangmanStages(int stageNum) {
+    if (stageNum == 0) {
+        return R"<|>(
+        ------
+       |      |
+       |       
+       |       
+       |       
+       |       
+       |        
+-----------------
+)<|>";
+    } else if (stageNum == 1) {
+        return R"<|>(
+        ------
+       |      |
+       |      O
+       |       
+       |       
+       |       
+       |       
+-----------------
+)<|>";
+    } else if (stageNum == 2) {
+        return R"<|>(
+        ------
+       |      |
+       |      O
+       |      |
+       |      |
+       |       
+       |       
+-----------------
+)<|>";
+    } else if (stageNum == 3) {
+        return R"<|>(
+        ------
+       |      |
+       |      O
+       |     \|
+       |      |
+       |       
+       |       
+-----------------
+)<|>";
+    } else if (stageNum == 4) {
+        return R"<|>(
+        ------
+       |      |
+       |      O
+       |     \|/
+       |      |
+       |       
+       |       
+-----------------
+)<|>";
+    } else if (stageNum == 5) {
+        return R"<|>(
+        ------
+       |      |
+       |      O
+       |     \|/
+       |      |
+       |     / 
+       |       
+-----------------
+)<|>";
+    } else if (stageNum == 6) {
+        return R"<|>(
+        ------
+       |      |
+       |      O
+       |     \|/
+       |      |
+       |     / \
+       |       
+-----------------
+)<|>";
+    }
+}
+
+// Only return by reference when the value outlives the function
+const std::string& randPhrase() {
+    static const std::array<std::string, 1> phrases = {"test phrase"};
+    return phrases[rand() % phrases.size()];
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::string rphrase = randPhrase();
+    PhraseClass phrase(rphrase);
+    int stage = 0;
+    bool quit = false;
+
+    do {
+        std::cout << "\033[2J\033[1;1H";
+        std::string input;
+        std::cout << hangmanStages(stage) << "\n";
+        std::cout << phrase.display() << "\n\n";
+
+        if (phrase.allFound()) {
+            std::cout << "You won!\n";
+            quit = true;
+            continue;
+        }
+
+        if (stage == 6) {
+            std::cout << "You lost.\n";
+            quit = true;
+        }
+        else {
+            std::cout << "To quit input: |\n";
+            std::cout << "Guess: ";
+            std::cin >> input;
+            std::cout << "\n";
+
+            if (input.length() > 1) {
+                std::string x;
+                std::cout << "Please only input one character.\n";
+                std::cout << "Type something and press enter to continue...";
+                std::cin >> x;
+                std::cout << "\n";
+            }
+            else {
+                char inputChar = input[0];
+                if ((inputChar >= 'A' && inputChar <= 'Z') || (inputChar >= 'a' && inputChar <= 'z')) {
+                    char inputLower = std::tolower(inputChar);
+                    stage += phrase.guess(inputLower);
+                }
+                else if (inputChar == '|') {
+                    quit = true;
+                }
+                else {
+                    std::string x;
+                    std::cout << "Please only input letters or | to quit.\n";
+                    std::cout << "Type something and press enter to continue...";
+                    std::cin >> x;
+                    std::cout << "\n";
+                }
+            }
+        }
+    } 
+    while (!quit);
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
